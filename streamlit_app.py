a99a312ef769f86c4b6ca550d05ef23dfd734ad7
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-11-24 09:45:54 -0700

    Update streamlit_app.py
    add a text entry box and send to input to fruityvice as part of the api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # write your own comment -what does the next line do? IT should return what fruit is actually selected and normalize it that way?
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? changes the data frame to match what is actually chosen 
 streamlit.dataframe(fruityvice_normalized)
+
+
