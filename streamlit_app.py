f6a110bcf5e09fb0d70431ad66576ec2a265777b
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-11-24 09:43:41 -0700

    Update streamlit_app.py
    deleted raw json text

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
 
 # write your own comment -what does the next line do? IT should return what fruit is actually selected and normalize it that way?
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
