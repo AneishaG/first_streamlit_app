bdee5d71ae1b605a1e0e08d2639eb6bab2b507b5
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-11-24 09:40:59 -0700

    Update streamlit_app.py
    Testing what the JSON does

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? IT should return what fruit is actually selected and normalize it that way?
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do? changes the data frame to match what is actually chosen 
+streamlit.dataframe(fruityvice_normalized)
