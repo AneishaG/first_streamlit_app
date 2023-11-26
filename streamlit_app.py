42915c2ed19217899fedd90753858d1bb60f4806
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-11-26 12:12:03 -0700

    Update streamlit_app.py
    adding text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
+#allow end user to add a fruit to the list 
+add_my_fruit = st.text_input ('What fruit would you like to add?')
+#st.write('The current movie title is', title)
