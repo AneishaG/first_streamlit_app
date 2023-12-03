88f5a7ce44fc80e34eb585b4307ab83870db8ed7
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-12-03 08:07:17 -0700

    Update streamlit_app.py
    troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+#from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -46,21 +46,21 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #not running any code past here while we trouble shoot 
-streamlit.stop()
+#streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 #allow end user to add a fruit to the list 
-add_my_fruit = st.text_input ('What fruit would you like to add?', 'Jackfruit')
-st.write('Thanks for adding', add_my_fruit)
+#add_my_fruit = st.text_input ('What fruit would you like to add?', 'Jackfruit')
+#st.write('Thanks for adding', add_my_fruit)
 
 #this will not work correctly but we are going with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit;)")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit;)")
