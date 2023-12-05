954edc00f45681cac089193163de5d46be9a4999
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-12-05 10:05:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,18 +62,18 @@ try:
 #not running any code past here while we trouble shoot 
 #streamlit.stop()
 
-#streamlit.header('WORDS')
+streamlit.header('WORDS')
 #snowflake-related functions
-#def get_fruit_load_list():
- #   with my_cnx.cursor() as my_cur:
-  #       my_cur.execute("select * from fruit_load_list")
-   #      return my_cur.fetchall()
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 
-#add a button to load the fruit
-#if streamlit.button('Get Fruit Load List'):
- #   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
-  #  my_data_rows = get_fruit_load_list()
-   # streamlit.dataframe(my_data_rows)
+add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 
 
@@ -81,8 +81,19 @@ try:
 
 
 #allow end user to add a fruit to the list 
-#add_my_fruit = streamlit.text_input ('What fruit would you like to add?', 'Jackfruit')
-#streamlit.write('Thanks for adding', add_my_fruit)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "thanks for adding " + new_fruit
 
-#this will not work correctly but we are going with it for now
-#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+add_my_fruit = streamlit.text_input ('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+
+streamlit.write('Thanks for adding', add_my_fruit)
+
+
+
