2c0deef123cbe77b5b1f21793548f8b221fc42f3
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-12-05 08:26:15 -0700

    Update streamlit_app.py
    adding else if statement into app

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,16 +34,22 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+#removed from api response
+#streamlit.write('The user entered ', fruit_choice)
 
 # write your own comment -what does the next line do? IT should return what fruit is actually selected and normalize it that way?
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do? changes the data frame to match what is actually chosen 
-streamlit.dataframe(fruityvice_normalized)
+
 
 #not running any code past here while we trouble shoot 
 streamlit.stop()
