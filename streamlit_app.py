1e7b2b29a611be26aaec9ae3530e0791dcb5b0bb
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-12-05 09:53:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ try:
 #not running any code past here while we trouble shoot 
 #streamlit.stop()
 
-streamlit.header('WORDS')
+#streamlit.header('WORDS')
 #snowflake-related functions
 #def get_fruit_load_list():
  #   with my_cnx.cursor() as my_cur:
