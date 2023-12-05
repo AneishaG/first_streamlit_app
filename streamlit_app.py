c9e9b17ed10eec92b7985f9061fcf55094fc4ab6
AneishaG <150881504+AneishaG@users.noreply.github.com>
2023-12-05 09:49:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,12 +60,12 @@ try:
 # write your own comment - what does this do? changes the data frame to match what is actually chosen 
 
 #not running any code past here while we trouble shoot 
-streamlit.stop()
+#streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
-    with my_cnx.curson() as my_cur:
+    with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
          return my_cur.fetchall()
 
